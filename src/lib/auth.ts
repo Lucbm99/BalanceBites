4ebf11b2cd9bc8fdb13080b47156ef2921cc1d9b
Lucbm99 <lbaradelmarchiori@gmail.com>
2025-03-10 21:23:31 -0300

    Feature - adicionando autenticação e persistindo ela no banco de dados

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,13 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
+import { db } from "@/db/drizzle";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub, Google],
   pages: {
-    newUser: "/",
     signIn: "/auth/login",
   },
   callbacks: {
